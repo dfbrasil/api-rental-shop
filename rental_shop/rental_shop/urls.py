a57a9cb7c10af78b614c0926fa5be0b225c53e73
Lucas <lucas@solarz.com.br>
2023-02-03 09:53:27 -0300

    registrando rotas

--- rental_shop/rental_shop/urls.py
+++ rental_shop/rental_shop/urls.py
@@ -1,21 +1,35 @@
-"""rental_shop URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.urls import include
 from django.urls import path
+from rest_framework import routers
+from django.conf import settings
+from django.conf.urls.static import static
+from rental_shop.carrinho.api.viewsets import CarrinhoViewSet
+from rental_shop.endereco.api.viewsets import EnderecoViewSet
+# from rental_shop.estoque.api.viewsets import EstoqueViewSet
+from rental_shop.item.api.viewsets import ItemViewSet
+from rental_shop.item_pedido.api.viewsets import ItemPedidoViewSet
+from rental_shop.locador.api.viewsets import LocadorViewSet
+from rental_shop.locatario.api.viewsets import LocatarioViewSet
+from rental_shop.transportadora.api.viewsets import TransportadoraViewSet
+from rental_shop.usuario.api.viewsets import UsuarioViewSet
+# from rest_framework.authtoken.views import obtain_auth_token
+
+router = routers.DefaultRouter()
+router.register(r'carrinhos', CarrinhoViewSet)
+router.register(r'enderecos', EnderecoViewSet)
+# router.register(r'estoques', EstoqueViewSet)
+router.register(r'itens', ItemViewSet)
+router.register(r'itemPedidos', ItemPedidoViewSet)
+router.register(r'locadores', LocadorViewSet)
+router.register(r'locatarios', LocatarioViewSet)
+router.register(r'transportadoras', TransportadoraViewSet)
+router.register(r'usuarios', UsuarioViewSet)
+
 
 urlpatterns = [
+    path('', include(router.urls)),
     path("admin/", admin.site.urls),
-]
+    # path('api-token-auth/', obtain_auth_token),
+] #+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
